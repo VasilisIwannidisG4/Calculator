be80a6778f0d38793cd69916923d499cd25bc6aa
User <User@DESKTOP-D4EGAOA>
2016-07-07 21:52:30 +0300

    new commit

---
+++ src/javatryout/JavaTryOut.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatryout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class JavaTryOut {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        startProgramHandler();
+    }
+
+    private static void startProgramHandler() {
+        
+    }
+    
+    private static void ReadNumber() {
+        
+    }
+    
+    private static void ReadCalculation() {
+        
+    }
+    
+    private static void SaveNumbers(double number) {
+        List<Double> numbers = new ArrayList();
+        
+        numbers.add(number);
+    }
+    /* 
+        code for scanf :
+            Scanner scanIn = new Scanner(System.in);
+            sWhatever = scanIn.nextLine();
+    */
+    private static void SaveCalculation(String calc) {
+        List<String> calculation = new ArrayList();
+        
+        calculation.add(calc);
+    }
+    
+    private static void Calculate() {
+        
+    }
+    
+    private static void PrintResults() {
+        
+    }
+}
